0f9448b0c9e631d84d20bb8e4ac41c133551e7fa
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2025-02-03 10:50:41 +0200

    update baseet

---
+++ src/app/program/page.jsx
@@ -0,0 +1,8 @@
+import Programs from '@/components/programs/Programs'
+import React from 'react'
+export default function page() { // Defining the main functional component named 'Footer'.
+
+    return (
+        <Programs />
+    )
+}
