6dda6e0eb0925030bf994ef19fb84276756a6faf
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2025-06-19 15:36:15 +0300

    update

--- src/components/achievements/Gallery.jsx
+++ src/components/achievements/Gallery.jsx
@@ -7,12 +7,14 @@ import BlurFade from '../ui/blur-fade';
 
 export default function Gallery({ data, googleDriveUrl }) {
     useEffect(() => {
-        Fancybox.bind("[data-fancybox]", {});
+        const timeout = setTimeout(() => {
+            Fancybox.bind("[data-fancybox]", {});
+        }, 100);  // 100ms delay
         return () => {
             Fancybox.destroy();
+            clearTimeout(timeout);
         }
-    }, []);
-    console.log(data);
+    }, [data]);
 
     return (
         <section className="gallery" id='gallery'>
@@ -20,45 +22,45 @@ export default function Gallery({ data, googleDriveUrl }) {
                 <section id="photos">
 
                     <div className="columns-2 gap-4 sm:columns-3">
-
                         {/* Embed Google Drive iFrame */}
 
-                        {data.images.map((img, idx) => {
-                            const isVideo = img.endsWith('.mp4') || img.endsWith('.webm') || img.endsWith('.mov');
-                            return (
-                                <BlurFade key={idx} delay={0.25 + idx * 0.05} inView>
-                                    <a href={img} data-fancybox="gallery">
-                                        {isVideo ? (
-                                            <video
+                        {data.images.map((img, idx) =>
+                            <BlurFade key={idx} delay={0.25 + idx * 0.05} inView>
+                                <a href={img} data-fancybox="gallery">
+                                    {img.endsWith('.mp4') || img.endsWith('.webm') || img.endsWith('.mov') ? (
+                                        <video
+                                            src={img}
+                                            className="mb-4 w-full max-h-96 rounded-3xl object-cover aspect-auto"
+                                            controls
+                                        />
+                                    ) : (
+                                        <figure>
+                                            <Image
                                                 src={img}
+                                                alt="Ataa"
+                                                width={200}
+
+                                                height={200}
                                                 className="mb-4 w-full max-h-96 rounded-3xl object-cover aspect-auto"
-                                                controls
                                             />
-                                        ) : (
-                                            <figure>
-                                                <Image
-                                                    src={img}
-                                                    alt="Ataa"
-                                                    width={200}
-                                                    height={200}
-                                                    className="mb-4 w-full max-h-96 rounded-3xl object-cover aspect-auto"
-                                                />
-                                            </figure>
-                                        )}
-                                    </a>
-                                </BlurFade>
-                            );
-                        })}
+                                        </figure>
+                                    )}
+                                </a>
+                            </BlurFade>
+
+                        )}
                         {
-                            data.videosUrl.map((video, idx) =>
-                                <BlurFade delay={0.25 + data.images.length * 0.05} inView key={idx}>
-                                    <div className="mb-4 w-full max-h-96 rounded-3xl overflow-hidden">
+                            data.videosUrl.map((video, index) =>
+                                <BlurFade delay={0.25 + data.images.length * 0.05} inView key={index}>
+                                    <div className="mb-4 w-full max-h-96 rounded-3xl overflow-hidden relative">
+                                        <label htmlFor={`video-${index}`} className="absolute top-0 left-0 w-full h-[20%]"></label>
                                         <iframe
                                             src={video.url.split('/view')[0] + '/preview'}
-                                            width="100%"
-                                            height="400"
+                                            // width="100"
+                                            // height="400"
                                             allow="autoplay"
                                             className="w-full h-96 rounded-3xl"
+                                            id={`video-${index}`}
                                         ></iframe>
                                     </div>
                                 </BlurFade>
